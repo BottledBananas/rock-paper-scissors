b4588c6868f02b71dcc5a8c13851e1f0c672f370
BottledBananas <rubentiehuis@msn.com>
2022-07-27 16:02:33 +0200

    added score counter and 5 round game

--- app.js
+++ app.js
@@ -32,7 +32,28 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  const playerSelection = prompt("Rock, paper or scissors?");
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  let computerScore = 0;
+  let playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock, paper or scissors?");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+
+    if (playerSelection != computerSelection) {
+      if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+          computerScore++;
+        } else playerScore++;
+      } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+          computerScore++;
+        } else playerScore++;
+      } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+          computerScore++;
+        } else playerScore++;
+      }
+    }
+  }
+  return `Player scored ${playerScore} points, Computer scored ${computerScore} points!`;
 }
